13b20fcbe6ef6ba8b94257fad6060e73dc64b56a
alinelx <alinelx@yahoo.com.br>
2025-11-04 02:05:25 +0000

    Nível Mestre Aline Lopes Xavier 202501055478

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     int tabuleiro[10][10];
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     int naviov[3] = {3,3,3};
     int navioh[3] = {3,3,3};
     int naviod1[3] = {3,3,3};
     int naviod2[3] = {3,3,3}; // tamanho dos navios vertical, horizontal e diagonais
-    int pivlinha, pivcol, pihlinha, pihcol, pid1linha, pid1col, pid2linha, pid2col; // posições iniciais
-    int i,j; // auxiliares
+    int habcone[3][5] = {
+        {5,5,1,5,5},
+        {5,1,1,1,5},
+        {1,1,1,1,1}
+    };
+    int habcruz[3][5] = {
+        {5,5,1,5,5},
+        {1,1,1,1,1},
+        {5,5,1,5,5}
+    };
+    int haboctaedro[3][3] = {
+        {5,1,5},
+        {1,1,1},
+        {5,1,5}
+    };
+    int pivlinha, pivcol, pihlinha, pihcol, pid1linha, pid1col, pid2linha, pid2col; // posições iniciais navios
+    int pihconelinha, pihconecol, pihcruzlinha, pihcruzcol, pihoctaedrolinha, pihoctaedrocol; //posições iniciais habilidades
+    int i,j,k,l; // auxiliares
 
     for (i=0;i<10;i++){ // Iniciar todas as posições como água "0", vertical
         for (j=0;j<10;j++){ // Iniciar todas as posições como água "0", horizontal
@@ -30,10 +39,21 @@ int main() {
     pid1col = 5;
     pid2linha = 9;
     pid2col = 0;
+    pihconelinha = 3;
+    pihconecol = 2;
+    pihcruzlinha = 1;
+    pihcruzcol = 4;
+    pihoctaedrolinha = 8;
+    pihoctaedrocol = 4;
 
 
-    if (pivlinha>7 || pihcol>7 || pid1col>7 || pid1linha >7 || pid2col>7 || pid2linha<2){
-        printf("Posição inicial ultrapassa tabuleiro");
+    if (pivlinha > 7 || pihcol > 7 || pid1col > 7 || pid1linha > 7 || pid2col > 7 || pid2linha<2){
+        printf("Posição inicial de navio ultrapassa tabuleiro");
+        return 0;
+    }
+
+    if (pihconelinha < 1 || pihconelinha > 7 || pihconecol < 2 || pihconecol > 7 || pihcruzlinha < 1 || pihcruzlinha > 8 || pihcruzcol < 2  || pihcruzcol > 7  || pihoctaedrolinha < 1 || pihoctaedrolinha > 8 || pihoctaedrocol < 1 || pihoctaedrocol > 8){
+        printf("Posição inicial de habilidade ultrapassa tabuleiro");
         return 0;
     }
 
@@ -54,10 +74,20 @@ int main() {
                         if(tabuleiro[pid2col+j][pid2linha+j]!=0){
                             printf("Posição sobreposta no navio diagonal 2");
                             return 0;
+                        }else{
+                            if(tabuleiro[pihconecol+j][pihconelinha+j]!=0){
+                                printf("Posiçãao sobreposta na habilidade cone");
+                                return 0;
+                            }else{
+                                if(tabuleiro[pihcruzcol+j][pihcruzlinha+j]!=0){
+                                printf("Posiçãao sobreposta na habilidade cruz");
+                                return 0;
+                                }
+                            }
                         }
                     }
                 }
-             }
+            }
         }
     }
 
@@ -83,16 +113,53 @@ int main() {
                 tabuleiro[i-1][j+1]=naviod2[1];
                 tabuleiro[i-2][j+2]=naviod2[2];
             }
+            if(i==pihconelinha && j==pihconecol){
+                for(k=0;k<3;k++){
+                    for(l=0;l<5;l++){
+                        tabuleiro[i][j-2]=habcone[k][l];
+                        j++;
+                    }
+                    j=pihconecol;
+                    i++;
+                }
+                i=pihconelinha;
+                j=pihconecol+1;
+            }
+            if(i==pihcruzlinha && j==pihcruzcol){
+                for(k=0;k<3;k++){
+                    for(l=0;l<5;l++){
+                        tabuleiro[i-1][j-2]=habcruz[k][l];
+                        j++;
+                    }
+                    j=pihcruzcol;
+                    i++;
+                }
+                i=pihcruzlinha;
+                j=pihcruzcol+1;
+            }
+            if(i==pihoctaedrolinha && j==pihoctaedrocol){
+                for(k=0;k<3;k++){
+                    for(l=0;l<3;l++){
+                        tabuleiro[i-1][j-1]=haboctaedro[k][l];
+                        j++;
+                    }
+                    j=pihoctaedrocol;
+                    i++;
+                }
+                i=pihoctaedrolinha;
+                j=pihoctaedrocol+1;
+            }
         }
     }
 
     for (i=0;i<10;i++){
         for (j=0;j<10;j++){
-            printf("%d",tabuleiro[i][j]);
+            printf("%d ",tabuleiro[i][j]);
         }
         printf("\n");
     }
-
+    return 0;
+}
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
@@ -118,6 +185,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
